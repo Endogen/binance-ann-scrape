abe039bbd7f07e090c6366a68b21bbfd650f4d89
endogen <ramodroll>
2020-07-22 11:36:06 +0200

    Reworked logic

--- announcements.py
+++ announcements.py
@@ -3,18 +3,36 @@
 import time
 import datetime
 import requests
-
+import threading
 
 from bs4 import BeautifulSoup
-from telegram.ext import Updater
+from telegram.ext import Updater, CommandHandler
 
 BASE_URL = "https://www.binance.com"
 
-news_str = str()
+check = True
+cache = list()
+updtr = Updater("891197415:AAEyQNI2zxf0cBkGg2t0IUVDGcWDcI7JTFk")
 
-updater = Updater("891197415:AAEyQNI2zxf0cBkGg2t0IUVDGcWDcI7JTFk")
 
-while True:
+def stop(bot, update):
+    def _shutdown():
+        global check
+        check = False
+
+        updtr.stop()
+        updtr.is_idle = False
+
+    update.message.reply_text("Stopping...")
+    threading.Thread(target=_shutdown).start()
+
+
+updtr.dispatcher.add_handler(CommandHandler(stop.__name__, stop))
+updtr.bot.send_message(134166731, "Starting...")
+updtr.start_polling(clean=True)
+
+init = True
+while check:
     time.sleep(2)
 
     response = requests.get(f"{BASE_URL}/en/support/announcement")
@@ -24,22 +42,27 @@ while True:
         continue
 
     soup = BeautifulSoup(response.content, "html.parser")
-    news = soup.find_all(class_="css-sbrje5")[0]
-    text = news.get_text()
 
-    print(f"[{datetime.datetime.now()}]", text, BASE_URL + news["href"])
+    for news in soup.find_all(class_="css-sbrje5"):
+        text = news.get_text()
 
-    if not news_str:
-        news_str = text
-        continue
+        if init:
+            cache.append(text)
+            continue
 
-    if news_str == text:
-        continue
+        if text in cache:
+            continue
+        else:
+            print(datetime.datetime.now(), "NEW ENTRY", text)
+            url = BASE_URL + news["href"]
 
-    url = BASE_URL + news["href"]
+            try:
+                updtr.bot.send_message(134166731, f"{text}\n\n{url}")
+                updtr.bot.send_message(1055475333, f"{text}\n\n{url}")
+            except Exception as e:
+                print(f"ERROR: {e}")
 
-    try:
-        updater.bot.send_message(134166731, f"{text}\n\n{url}")
-        updater.bot.send_message(1055475333, f"{text}\n\n{url}")
-    except:
-        pass
+        cache.append(text)
+
+    init = False
+    print(datetime.datetime.now(), "Up to date")
