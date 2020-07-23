53564df2be83fc7cc99f0db79384885b4275e7e3
endogen <ramodroll>
2020-07-23 17:05:52 +0200

    Added some new commands

--- announcements.py
+++ announcements.py
@@ -2,6 +2,7 @@
 
 import sys
 import time
+import logging
 import datetime
 import requests
 import threading
@@ -11,6 +12,7 @@ from telegram.ext import Updater, CommandHandler
 
 BASE_URL = "https://www.binance.com"
 
+last = str()
 check = True
 cache = list()
 updtr = Updater("891197415:AAEyQNI2zxf0cBkGg2t0IUVDGcWDcI7JTFk")
@@ -32,42 +34,62 @@ def stop(bot, update):
     threading.Thread(target=_shutdown).start()
 
 
+def state(bot, update):
+    update.message.reply_text(f"Happily running...\n\n{last}")
+
+
+def sleep(bot, update, args):
+    try:
+        global wait
+        wait = float(args[0])
+        update.message.reply_text("Done")
+    except Exception as ex:
+        update.message.reply_text(f"ERROR: {ex}")
+
+
 updtr.dispatcher.add_handler(CommandHandler(stop.__name__, stop))
+updtr.dispatcher.add_handler(CommandHandler(state.__name__, state))
+updtr.dispatcher.add_handler(CommandHandler(sleep.__name__, sleep, pass_args=True))
+
 updtr.bot.send_message(134166731, "Starting...")
 updtr.start_polling(clean=True)
 
 init = True
 while check:
-    time.sleep(wait)
+    try:
+        time.sleep(wait)
 
-    response = requests.get(f"{BASE_URL}/en/support/announcement", {"timeout": wait})
+        response = requests.get(f"{BASE_URL}/en/support/announcement", {"timeout": wait})
 
-    if response.status_code != 200:
-        print("Status code " + str(response.status_code))
-        continue
-
-    soup = BeautifulSoup(response.content, "html.parser")
-
-    for news in soup.find_all(class_="css-sbrje5"):
-        text = news.get_text()
-
-        if init:
-            cache.append(text)
+        if response.status_code != 200:
+            print("Status code " + str(response.status_code))
             continue
 
-        if text in cache:
-            continue
-        else:
-            print(datetime.datetime.now(), text)
-            url = BASE_URL + news["href"]
+        soup = BeautifulSoup(response.content, "html.parser")
 
-            try:
+        for news in soup.find_all(class_="css-sbrje5"):
+            text = news.get_text()
+
+            if init:
+                cache.append(text)
+                continue
+
+            if text in cache:
+                continue
+            else:
+                last = f"[{datetime.datetime.now()}] {text}"
+                print(last)
+
+                url = BASE_URL + news["href"]
+
                 updtr.bot.send_message(134166731, f"{text}\n\n{url}")
                 updtr.bot.send_message(1055475333, f"{text}\n\n{url}")
-            except Exception as e:
-                print(f"ERROR: {e}")
 
-        cache.append(text)
+            cache.append(text)
 
-    init = False
-    print(datetime.datetime.now(), "Up to date")
+        init = False
+        print(datetime.datetime.now(), "Up to date")
+    except Exception as e:
+        msg = f"ERROR: {e}"
+        logging.error(msg)
+        updtr.bot.send_message(134166731, msg)
